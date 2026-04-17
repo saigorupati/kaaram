88faa9f2a5355cf10f6f6001c7fcf2bed366e47d
Sai Gorupati <sainikit.gorupati@gmail.com>
2026-04-17 12:06:50 -0700

    Initial Commit

---
+++ kaaram/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  kaaram
+//
+//  Created by Sai Gorupati on 4/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ kaaramTests/kaaramTests.swift
@@ -0,0 +1,17 @@
+//
+//  kaaramTests.swift
+//  kaaramTests
+//
+//  Created by Sai Gorupati on 4/17/26.
+//
+
+import Testing
+@testable import kaaram
+
+struct kaaramTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
